5c58041d9b5c6da039018c5537ab8cbddbd59856
victorCNeves <105159565+victorCNeves@users.noreply.github.com>
2026-04-18 18:01:29 -0300

    feat: lógica de salvar produto cadastrado no formulário

---
+++ REA-Conteudo/codigo-ecommerce/formulario/script.js
@@ -0,0 +1,17 @@
+const salvarProduto = (event) => {
+    event.preventDefault();
+    const produtos = JSON.parse(localStorage.getItem('produtos') || "{}");
+    const dados = Object.fromEntries(new FormData(event.target));
+    const id = getId();
+    console.log(dados);
+    produtos[id] = {id: id, ...dados};
+    console.log(produtos[id]);
+    localStorage.setItem('produtos',JSON.stringify(produtos));
+    event.target.reset();
+}
+
+const getId = () => {
+    const id = parseInt(localStorage.nextId || '0');
+    localStorage.setItem('nextId',`${id+1}`);
+    return id;
+}
