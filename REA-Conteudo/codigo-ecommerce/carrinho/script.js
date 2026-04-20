8507624b13f065e7d5cb1206df95d545c67540c4
victorCNeves <105159565+victorCNeves@users.noreply.github.com>
2026-04-19 23:55:30 -0300

    feat: renderização do carrinho na tela + mudança minima na lógica de oculto

--- REA-Conteudo/codigo-ecommerce/carrinho/script.js
+++ REA-Conteudo/codigo-ecommerce/carrinho/script.js
@@ -8,11 +8,53 @@ const verificarCarrinhoVazio = () => {
     const carrinhoVazio = document.querySelector('.carrinho-vazio');
     const resumoCarrinho = document.querySelector('.resumo-carrinho');
 
-    if(carrinho.length==0){
-        carrinhoVazio.classList.remove('oculto');
+    resumoCarrinho.classList.remove('oculto');
+    carrinhoVazio.classList.remove('oculto');
+    if (carrinho.length == 0) {
         resumoCarrinho.classList.add('oculto');
-    }else{
+    } else {
         carrinhoVazio.classList.add('oculto');
-        resumoCarrinho.classList.remove('oculto');
+        carregarCarrinho(carrinho);
     }
+}
+
+const carregarCarrinho = (carrinho) => {
+    const produtosTotal = JSON.parse(localStorage.getItem('produtos'));
+    const produtos = carrinho.map(e => {
+        const produto = produtosTotal[e.id_produto];
+        produto['quantidade'] = e.quantidade
+        produto['preco'] = parseFloat(produto.preco)
+        produto['valor_total'] = e.quantidade * produto.preco
+        return produto;
+    });
+
+    const itensCarrinho = document.querySelector('.itens-carrinho')
+    const fragment = document.createDocumentFragment();
+    let valorTotal = 0;
+    produtos.forEach((e) => {
+        const item = document.createElement('div');
+        item.classList.add('item')
+        const string = `
+            <img src="${e.foto}" alt="${e.nome}">
+
+            <div class="item-detalhes">
+                <h3>${e.nome}</h3>
+                <p class="preco-unitario">R$ ${e.preco.toFixed(2)}</p>
+
+                <div class="item-acoes">
+                    <input type="number" value="${e.quantidade}" min="1" class="qtd-input">
+                    <button class="btn-remover">Excluir</button>
+                 </div>
+             </div>
+
+            <div class="item-total">
+                <p class="preco-subtotal">R$ ${e.valor_total.toFixed(2)}</p>
+             </div>
+        `;
+        item.innerHTML=string;
+        fragment.appendChild(item);
+        valorTotal += e.valor_total;
+    });
+    itensCarrinho.appendChild(fragment);
+    document.querySelector('.valor-total').textContent = `R$ ${valorTotal.toFixed(2)}`;
 }
