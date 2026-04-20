8d219abc9ba8ca03ae304e9df4f2217dbfa08b64
victorCNeves <105159565+victorCNeves@users.noreply.github.com>
2026-04-20 17:09:18 -0300

    feat: alterar quantidade de itens no carrinho + remover item do carrinho

--- REA-Conteudo/codigo-ecommerce/carrinho/script.js
+++ REA-Conteudo/codigo-ecommerce/carrinho/script.js
@@ -2,6 +2,26 @@ document.addEventListener('DOMContentLoaded',(e)=>{
     verificarCarrinhoVazio();
 });
 
+document.querySelector('.itens-carrinho').addEventListener('click',(e)=>{
+    if(e.target.className === 'btn-remover' || e.target.className === 'qtd-input'){
+        const id = e.target.parentElement.parentElement.parentElement.getAttribute('data-id');
+        if(e.target.className === 'btn-remover'){
+            removerProduto(id);
+        }
+    
+        if(e.target.className === 'qtd-input'){
+            alterarQuantidade(id, parseInt(e.target.value))
+        }
+    }
+});
+
+document.querySelector('.itens-carrinho').addEventListener('change',(e)=>{
+    if(e.target.className === 'qtd-input'){
+        const id = e.target.parentElement.parentElement.parentElement.getAttribute('data-id');
+        alterarQuantidade(id,parseInt(e.target.value));
+    }
+});
+
 const verificarCarrinhoVazio = () => {
     const carrinho = JSON.parse(localStorage.getItem('carrinho') || '[]');
 
@@ -10,6 +30,7 @@ const verificarCarrinhoVazio = () => {
 
     resumoCarrinho.classList.remove('oculto');
     carrinhoVazio.classList.remove('oculto');
+    limparTela();
     if (carrinho.length == 0) {
         resumoCarrinho.classList.add('oculto');
     } else {
@@ -22,9 +43,9 @@ const carregarCarrinho = (carrinho) => {
     const produtosTotal = JSON.parse(localStorage.getItem('produtos'));
     const produtos = carrinho.map(e => {
         const produto = produtosTotal[e.id_produto];
-        produto['quantidade'] = e.quantidade
-        produto['preco'] = parseFloat(produto.preco)
-        produto['valor_total'] = e.quantidade * produto.preco
+        produto['quantidade'] = e.quantidade;
+        produto['preco'] = parseFloat(produto.preco);
+        produto['valor_total'] = e.quantidade * produto.preco;
         return produto;
     });
 
@@ -37,7 +58,7 @@ const carregarCarrinho = (carrinho) => {
         const string = `
             <img src="${e.foto}" alt="${e.nome}">
 
-            <div class="item-detalhes">
+            <div class="item-detalhes"">
                 <h3>${e.nome}</h3>
                 <p class="preco-unitario">R$ ${e.preco.toFixed(2)}</p>
 
@@ -52,9 +73,32 @@ const carregarCarrinho = (carrinho) => {
              </div>
         `;
         item.innerHTML=string;
+        item.setAttribute('data-id',e.id)
         fragment.appendChild(item);
         valorTotal += e.valor_total;
     });
     itensCarrinho.appendChild(fragment);
     document.querySelector('.valor-total').textContent = `R$ ${valorTotal.toFixed(2)}`;
+}
+
+const limparTela = () => {
+    const itensCarrinho = document.querySelector('.itens-carrinho')
+    const itens = document.querySelectorAll('.item');
+    itens.forEach((e)=>itensCarrinho.removeChild(e));
+}
+
+const removerProduto = (id) => {
+    const carrinho = JSON.parse(localStorage.getItem('carrinho'));
+    localStorage.setItem('carrinho',JSON.stringify(carrinho.filter((e)=>e.id_produto!=id)));
+    verificarCarrinhoVazio();
+}
+
+const alterarQuantidade = (id, quantidade) => {
+    const carrinho = JSON.parse(localStorage.getItem('carrinho'));
+    const item = carrinho.find((e)=>e.id_produto==id);
+    if(item.quantidade!=quantidade){
+        carrinho.find((e)=>e.id_produto==id).quantidade = quantidade;
+        localStorage.setItem('carrinho',JSON.stringify(carrinho));
+        verificarCarrinhoVazio();
+    }
 }
