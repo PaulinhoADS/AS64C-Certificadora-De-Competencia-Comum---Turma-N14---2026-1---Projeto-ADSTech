857b51afed33bddffdef7e2098cb9ccfdfe05e7f
victorCNeves <105159565+victorCNeves@users.noreply.github.com>
2026-04-28 21:23:15 -0300

    fix: remove console.log()

--- REA-Conteudo/codigo-ecommerce/formulario/script.js
+++ REA-Conteudo/codigo-ecommerce/formulario/script.js
@@ -3,9 +3,7 @@ const salvarProduto = (event) => {
     const produtos = JSON.parse(localStorage.getItem('produtos') || "{}");
     const dados = Object.fromEntries(new FormData(event.target));
     const id = getId();
-    console.log(dados);
     produtos[id] = {id: id, ...dados};
-    console.log(produtos[id]);
     localStorage.setItem('produtos',JSON.stringify(produtos));
     event.target.reset();
     window.scrollTo({ top: 0, behavior: 'smooth' });
